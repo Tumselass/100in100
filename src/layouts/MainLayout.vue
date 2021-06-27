7798226f4505e308e06c1e4451ec586b0c395726
Thomas Flunes <tflunes@gmail.com>
2021-06-27 16:57:20 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> 100in100 </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {},
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,114 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn v-if="!chain.length" @click="createNewChain">START</q-btn>
+    <!-- {{ chain }} -->
+    {{ visableBlockDate }}
+
+    <span
+      :class="{ 'rep-count--complete': todaysRepsComplete }"
+      class="rep-count"
+      >{{ chain[visableBlockIndex]?.reps }}</span
+    >
+    <q-btn @click="addReps(10)">+10</q-btn>
+    <q-btn @click="addReps(20)">+20</q-btn>
+    <q-btn @click="addReps(30)">+30</q-btn>
+    <q-btn @click="addReps(40)">+40</q-btn>
+    <br />
+    <q-btn @click="goToBlockIndex(visableBlockIndex - 1)"
+      >prev{{ visableBlockIndex }}</q-btn
+    >
+    <q-btn @click="goToBlockIndex(todaysBlockIndex)">TODAY</q-btn>
+    <q-btn @click="goToBlockIndex(visableBlockIndex + 1)"
+      >next{{ chain.length - 1 - visableBlockIndex }}</q-btn
+    >
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, watchEffect } from 'vue';
+import startOfToday from 'date-fns/startOfToday';
+import format from 'date-fns/format';
+import { LocalStorage } from 'quasar';
+
+interface Chain {
+  day: number;
+  reps: number;
+}
+
+export default defineComponent({
+  name: 'PageIndex',
+
+  setup() {
+    const chain = ref<Chain[]>([]);
+
+    if (LocalStorage.has('pushups')) {
+      chain.value = LocalStorage.getItem('pushups') as Chain[];
+    }
+
+    watchEffect(() => LocalStorage.set('pushups', chain.value));
+
+    function createNewChain() {
+      const newChain = [];
+      for (let i = 0; i < 100; i++) {
+        const day: number = newChain.length
+          ? newChain[newChain.length - 1].day + 86400000
+          : startOfToday().getTime();
+        newChain.push({ day, reps: 0 });
+      }
+      chain.value = newChain;
+      LocalStorage.set('pushups', newChain);
+      visableBlockIndex.value = 0;
+    }
+
+    const todaysBlockIndex = computed(() => {
+      const today = startOfToday().getTime();
+      return chain.value.findIndex((block) => block.day === today);
+    });
+
+    const visableBlockIndex = ref(todaysBlockIndex.value);
+
+    function goToBlockIndex(index: number) {
+      if (index < 0 || index === 100) return;
+      visableBlockIndex.value = index;
+    }
+
+    const visableBlockDate = computed(() => {
+      if (visableBlockIndex.value === -1) return '';
+      return format(
+        chain.value[visableBlockIndex.value]?.day,
+        'EEEE d MMMM yyyy'
+      );
+    });
+
+    const todaysRepsComplete = computed(() => {
+      return chain.value[todaysBlockIndex.value]?.reps >= 100;
+    });
+
+    function addReps(amount: number) {
+      chain.value[visableBlockIndex.value].reps += amount;
+    }
+
+    return {
+      chain,
+      todaysBlockIndex,
+      visableBlockDate,
+      todaysRepsComplete,
+      visableBlockIndex,
+      goToBlockIndex,
+
+      createNewChain,
+      addReps,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.rep-count {
+  font-weight: bold;
+}
+
+.rep-count--complete {
+  color: $green;
+}
+</style>
